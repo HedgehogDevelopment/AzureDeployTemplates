dd908e83046137d9dd075916991094164c6913cc
Charles Turano <cturano@hhogdev.com>
2017-06-15 14:26:59 -0400

    Initial version of Files

---
+++ SC82U3/Install.ps1
@@ -0,0 +1,10 @@
+﻿#Import the stuff we need 
+Import-Module AzureRM
+Import-Module ..\Tools\Sitecore.Cloud.Cmdlets.psm1
+
+#Add the azure account
+Add-AzureRmAccount
+
+$Name="sc82u3-launch1"
+
+Start-SitecoreAzureDeployment -Location "East US" -Name $Name -ArmTemplateUrl "https://ctsitecorepackages.blob.core.windows.net/sitecore/xp/azuredeploy.json" -ArmParametersPath ".\azuredeploy.parameters.json" -LicenseXmlPath "C:\Charlie\Installs\Sitecore\license.xml" -Verbose
---
+++ SC82U3/ShrinkPlan.ps1
@@ -0,0 +1,30 @@
+﻿#Import the stuff we need 
+Import-Module AzureRM
+Import-Module ..\Tools\Sitecore.Cloud.Cmdlets.psm1
+
+#Add the azure account
+Add-AzureRmAccount
+
+$Name="sc82u3-launch1"
+
+$NewDatabaseEdition = "Standard";
+$NewDatabasePricingTier = "S0" # the default database tier of Sitecore XP provisioning script is S1
+$NewHostingPlanTier = "Basic"; # the default hosting plan tier of Sitecore XP provisioning script is Standard
+
+Write-Host "Group all databases into one server...";
+New-AzureRmSqlDatabaseCopy -ResourceGroupName $Name -ServerName "$Name-web-sql" -DatabaseName "$Name-web-db" -CopyServerName "$Name-sql" -CopyDatabaseName "$Name-web-db"
+Remove-AzureRmSqlServer -ResourceGroupName $Name -ServerName "$Name-web-sql" -Force
+
+Write-Host "Starting database scaling...";
+$DatabaseNames = "core-db", "master-db", "web-db", "reporting-db"; 
+Foreach ($DatabaseName in $DatabaseNames)
+{
+    Set-AzureRmSqlDatabase -DatabaseName "$Name-$DatabaseName" -ServerName "$Name-sql" -ResourceGroupName $Name -Edition $NewDatabaseEdition -RequestedServiceObjectiveName $NewDatabasePricingTier
+}
+
+Write-Host "Starting hosting plan scaling...";
+$HostingPlans = "cd-hp", "cm-hp", "prc-hp", "rep-hp";
+Foreach($HostingPlan in $HostingPlans)
+{
+    Set-AzureRmAppServicePlan -ResourceGroupName $Name -Name "$Name-$HostingPlan" -Tier $NewHostingPlanTier
+}
